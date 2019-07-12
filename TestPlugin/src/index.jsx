d6e999c9841e9eecadad90c82edfa96817d0d4c7
Sprachbot <sprachbot@CONET-FFM-SWEs-MacBook-Pro.local>
2019-07-12 14:31:17 +0200

    Initial commit

---
+++ TestPlugin/src/index.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+const RedText = (props) => {
+    const text = props.message.text;
+
+    return (
+        <span style={{color: 'red'}}>
+            {text}
+        </span>
+    );
+}
+
+const redTextPlugin = {
+    match: 'red-text',
+    component: RedText
+}
+
+if (!window.cognigyWebchatMessagePlugins) {
+    window.cognigyWebchatMessagePlugins = [];
+}
+
+window.cognigyWebchatMessagePlugins.push(redTextPlugin);
