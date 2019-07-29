8182fbfd0ecea358a24ac075d228c8d60c9a3a43
Sprachbot <sprachbot@CONET-FFM-SWEs-MacBook-Pro.local>
2019-07-29 12:56:50 +0200

    Check location before opening WebChat

--- open-webchat.js
+++ open-webchat.js
@@ -1,4 +1,6 @@
-initWebchat('https://endpoint-demo.cognigy.ai/5cbe75a9701bc44dc658184755cfd131de02cd8aac81d4ad488163d333e8f72c')
-            .then(cognigyWebchat => {
-                cognigyWebchat.open();
-            });
+if (window.location.href.includes('portalcareer?correlation_Id=')) {
+    initWebchat('https://endpoint-demo.cognigy.ai/5cbe75a9701bc44dc658184755cfd131de02cd8aac81d4ad488163d333e8f72c')
+                .then(cognigyWebchat => {
+                    cognigyWebchat.open();
+                });
+}
