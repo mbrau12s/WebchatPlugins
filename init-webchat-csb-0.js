826f48a67535f658b7128e0b518a3a9630f77f5d
Sprachbot <sprachbot@CONET-FFM-SWEs-MacBook-Pro.local>
2019-08-02 14:08:01 +0200

    EventListener test

---
+++ init-webchat-csb-0.js
@@ -0,0 +1,10 @@
+// initWebchat('https://endpoint-demo.cognigy.ai/5cbe75a9701bc44dc658184755cfd131de02cd8aac81d4ad488163d333e8f72c')
+//     .then(cognigyWebchat => {
+//         window.cognigyWebchat = cognigyWebchat;
+//     });
+
+const cognigyWebchat = initWebchat('https://endpoint-demo.cognigy.ai/5cbe75a9701bc44dc658184755cfd131de02cd8aac81d4ad488163d333e8f72c');
+const keywordInput = document.getElementById('keywordsearch-q');
+const locationInput = document.getElementById('keywordsearch-locationsearch');
+
+keywordInput.addEventListener('click', () => { cognigyWebchat.open(); });
--- init-webchat-csb.js
+++
@@ -1,4 +0,0 @@
-initWebchat('https://endpoint-demo.cognigy.ai/5cbe75a9701bc44dc658184755cfd131de02cd8aac81d4ad488163d333e8f72c')
-    .then(cognigyWebchat => {
-        cognigyWebchat.open();
-    });
