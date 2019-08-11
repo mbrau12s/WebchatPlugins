42d0de7e68bd9bb4a20f57503ca047c7bd5fc41b
Manuel Braun <manuelbraun92@gmail.com>
2019-08-11 11:43:17 +0200

    Added ESLint

--- init-webchat-csb-12.js
+++ init-webchat-csb-12.js
@@ -1,18 +1,19 @@
-initWebchat('https://endpoint-demo.cognigy.ai/5cbe75a9701bc44dc658184755cfd131de02cd8aac81d4ad488163d333e8f72c')
-    .then(cognigyWebchat => {
-        window.cognigyWebchat = cognigyWebchat;
-    });
+initWebchat(
+  "https://endpoint-demo.cognigy.ai/5cbe75a9701bc44dc658184755cfd131de02cd8aac81d4ad488163d333e8f72c"
+).then(cognigyWebchat => {
+  window.cognigyWebchat = cognigyWebchat;
+});
 
-const content = document.getElementById('content');
-const form = document.createElement('form');
-form.id = 'my-form';
-const input = document.createElement('input');
-input.id = 'my-form-input';
-input.type = 'text';
+const content = document.getElementById("content");
+const form = document.createElement("form");
+form.id = "my-form";
+const input = document.createElement("input");
+input.id = "my-form-input";
+input.type = "text";
 form.appendChild(input);
 content.appendChild(form);
 
-input.onclick = (event) => {
-    event.preventDefault();
-    window.cognigyWebchat.open();
-}
+input.onclick = event => {
+  event.preventDefault();
+  window.cognigyWebchat.open();
+};
